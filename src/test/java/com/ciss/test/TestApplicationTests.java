ad43128abf2a8e93eb0ec480fc76d309475386c1
vkrh 0406 <vkrh0406@naver.com>
2024-10-01 05:55:18 +0900

    [init] init

---
+++ src/test/java/com/ciss/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciss.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
